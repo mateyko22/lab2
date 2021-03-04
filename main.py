cf5ff2e12919671357df7597061d0c5c1e0c1d15
mateyko22 <petmat@o2.pl>
2021-03-04 14:17:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from math import *
+# # Zad 1.
+# sporty = ['pilka nozna', 'siatkowka', 'koszykowka', 'skoki narciarskie']
+# print(sporty)
+# sporty.reverse()
+# print(sporty)
+# sporty.append('hokej')
+# sporty.append('pilka reczna')
+# print(sporty)
+
+# Zad 2.
+# slownik = {'np': 'na przyklad', 'jd': 'jest dobrze', 'zw': 'zaraz wracam'}
+# print(slownik['jd'])
+
+# Zad 3.
+# gry = {'CS': 'Counter-Strike', 'LoL': 'League of Legends', 'BF': 'Battlefield', 'WoT': 'World of Tanks',
+#        'WoW': 'World of Warcraft'}
+# print("Liczba elementow w slowniku:", len(gry))
+
+# Zad 4.
+# zdanie = input("Wprowadz zdanie: ")
+# ile = 0
+# for licznik in range(0, len(zdanie), 1):
+#     if zdanie[licznik] == 'a':
+#         ile += 1
+# print("Liczba wystąpień litery a: ",ile)
+
+# Zad 6.
+# a = input("Podaj a:")
+# b = input("Podaj b:")
+# c = input("Podaj c:")
+# maks = a
+# if b > a or c > a:
+#     if b > c:
+#         maks = b
+#     elif c > b:
+#         maks = c
+# print(maks)
+
+# Zad 7.
+# lista = [2, 5, 7.5, 4, 10.1, 1.24, 3]
+# for x in lista:
+#     print(pow(x, 2))
+
+# Zad 10.
+# a = input("Podaj liczbe: ")
+# a = int(a)
+# if a < 0:
+#     print("a nie może być ujemne.")
+# else:
+#     pierwiastek = sqrt(a)
+#     print("Pierwiastek z podanej liczby:", pierwiastek)
